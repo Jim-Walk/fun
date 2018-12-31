bd9ee5db5b2c6a05bf7649d540d73d4808e4094d
andrewpsuedonym <j.m.walker@live.co.uk>
2018-12-31 16:29:36 +0000

    wave is how I like it

--- wave/sketch.js
+++ wave/sketch.js
@@ -1,36 +1,38 @@
 let angle = 0;
 let ma; // magic angle to make things look nice
 let maxD;
+let mat = 0;
 
 function setup(){
     createCanvas(400,400, WEBGL);
-    ma = atan(1/sqrt(2));
+    //ma = atan(1/sqrt(2));
+    ma = atan(cos(QUARTER_PI));
     maxD = dist(0,0,200,200)
 }
 
 function draw(){
     background(100);
 
-    ortho(-300, 300, 300, -300, -200, 1000);
-    directionalLight(255, 255, 255, 0, -1, 0);
+    ortho(-400, 400, 400, -400, 0, 1000);
+    directionalLight(204, 255, 255, 1, -1, 0);
 
-    rotateX(-ma);
+    rotateX(ma);
     rotateY(-PI/4);
 
-    let w = 32;
+    let w = 24;
     let offset = 0;
 
     for (let z = 0; z<height; z+=w){
         for (let x = 0; x<width; x+= w){
             push();
             let d = dist(x,z, width/2, height/2)
-            let offset = map(d, 0, maxD, -1, 1)
+            let offset = map(d, 0, maxD, -2, 2)
             let a = angle + offset;
-            let h = floor(map(sin(a), -1, 1, 20, 200));
+            let h = floor(map(sin(a), -1, 1, 75, 350));
             //normalMaterial();
-            ambientMaterial(255);
-            translate(x-width/2, 0, z);
-            box(w-2, h, w-2);
+            ambientMaterial(mat);
+            translate(x - width/2, 0, z-height/2 );
+	         box(w, h, w);
 
             fill(255);
             //rect(x-width/2 + w/2,0,w-2,h);
@@ -38,4 +40,5 @@ function draw(){
         }
     }
     angle -= 0.1;
+    mat += 1
 }
