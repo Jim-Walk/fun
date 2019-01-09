294441e777579e459b86ebd8f913a5538d340430
andrewpsuedonym <j.m.walker@live.co.uk>
2019-01-09 16:06:31 +0000

    draw points on lin regression

--- linear-regression/sketch.js
+++ linear-regression/sketch.js
@@ -16,5 +16,16 @@ function mousePressed(){
    let y = map(mouseY, 0, height, 0, 1);
    xs.push(x);
    ys.push(y);
+}
 
+function draw(){
+   background(0);
+   strokeWeight(4);
+   stroke(255);
+   // draw our points, scale back up from unit square
+   for (let i=0; i<xs.length; i++){
+        let px = map(xs[i], 0, 1, 0, width);
+        let py = map(ys[i], 0, 1, 1, height);
+        point(px,py);
+   }
 }
